5e0d31db7b469313a54c27f01e8ad282a86b8fa1
Samuel Alves <samalves.dev@gmail.com>
2025-08-01 00:46:20 -0300

    feat: Cria as entidades Conta e Transacao.

---
+++ src/main/java/apitransactionbank/bank/entities/Conta.java
@@ -0,0 +1,35 @@
+package apitransactionbank.bank.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Conta")
+public class Conta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long numeroConta;
+    private Float saldoConta;
+
+    public Conta(Long numeroConta, Float saldoConta) {
+        this.numeroConta = numeroConta;
+        this.saldoConta = saldoConta;
+    }
+
+    public Long getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(Long numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public Float getSaldoConta() {
+        return saldoConta;
+    }
+
+    public void setSaldoConta(Float saldoConta) {
+        this.saldoConta = saldoConta;
+    }
+
+}
---
+++ src/main/java/apitransactionbank/bank/entities/Transacao.java
@@ -0,0 +1,44 @@
+package apitransactionbank.bank.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Transacao")
+public class Transacao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String formaPagamento;
+    private Float valor;
+    private Long conta;
+
+    public Transacao(String formaPagamento, Float valor, Long conta) {
+        this.formaPagamento = formaPagamento;
+        this.valor = valor;
+        this.conta = conta;
+    }
+
+    public String getFormaPagamento() {
+        return formaPagamento;
+    }
+
+    public void setFormaPagamento(String formaPagamento) {
+        this.formaPagamento = formaPagamento;
+    }
+
+    public Float getValor() {
+        return valor;
+    }
+
+    public void setValor(Float valor) {
+        this.valor = valor;
+    }
+
+    public Long getConta() {
+        return conta;
+    }
+
+    public void setConta(Long conta) {
+        this.conta = conta;
+    }
+}
